b2269c4c7228ff4fc20820c58043ada680079d75
Mkenziq23 <kikic610@gmail.com>
2023-07-25 01:26:30 +0700

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Toggle Class Active//
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika humburger menu di klik
+document.querySelector('#humberger-menu').onclick=()=>{
+    navbarNav.classList.toggle('active');
+};
